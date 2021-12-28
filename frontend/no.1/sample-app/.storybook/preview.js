b75285f4a7a3b0e3e7d217a937b98d8ca35c6507
tsubasa <tsubasan1122@gmail.com>
2021-12-28 16:54:30 +0900

    next/imageの対応追加

--- frontend/no.1/sample-app/.storybook/preview.js
+++ frontend/no.1/sample-app/.storybook/preview.js
@@ -1,6 +1,15 @@
 // previewでもtailwind.cssを使用するために読み込む
 import '../styles/globals.css';
 
+// next/imageをstorybookで使用し、srcにURLを渡すとエラーになる
+import * as nextImage from 'next/image';
+
+Object.defineProperty(nextImage, 'default', {
+  configurable: true,
+  value: (props) => <img {...props} />,
+});
+//
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
