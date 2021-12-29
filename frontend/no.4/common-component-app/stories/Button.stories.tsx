d3458d73c4553d5f1bff1c7c1fcbdd84c6d9f491
tsubasa <tsubasan1122@gmail.com>
2021-12-29 19:24:12 +0900

    button storiesの修正

--- frontend/no.4/common-component-app/stories/Button.stories.tsx
+++ frontend/no.4/common-component-app/stories/Button.stories.tsx
@@ -12,38 +12,27 @@ export default {
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
-export const Red = Template.bind({});
-Red.args = {
-  children: '登録する',
-  color: 'red',
-  size: 'medium',
-  disabled: true,
-  onClick: () => {
-    console.log('登録しました');
-  },
-};
-
 export const Blue = Template.bind({});
 Blue.args = {
-  children: '登録する',
+  children: '応募する',
   color: 'blue',
   size: 'medium',
-  disabled: true,
+  disabled: false,
   onClick: () => {},
 };
 
-export const Green = Template.bind({});
-Green.args = {
-  children: '登録する',
-  color: 'green',
-  size: 'medium',
-  disabled: true,
+export const Red = Template.bind({});
+Red.args = {
+  children: '削除する',
+  color: 'red',
+  size: 'small',
+  disabled: false,
   onClick: () => {},
 };
 
-export const Small = Template.bind({});
-Small.args = {
-  children: '登録する',
+export const RedDisabled = Template.bind({});
+RedDisabled.args = {
+  children: '削除する',
   color: 'red',
   size: 'small',
   disabled: true,
