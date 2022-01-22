aff3aef2f9187bd8208a559d796248b3af32768e
tsubasa <tsubasan1122@gmail.com>
2022-01-22 23:49:37 +0900

    airbnbのlintとhuskeyの設定を追加

--- チーム開発/no.1/lint-sample-app/src/App.js
+++ チーム開発/no.1/lint-sample-app/src/App.js
@@ -1,11 +1,15 @@
-import "./App.css";
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
         </p>
         <a
           className="App-link"
